c49775ed9bcb00a96a5bedd23683589b2d0d3f50
Edoardo <edfl.tunzi@gmail.com>
2024-12-16 11:44:41 +0100

    searchbar. logo, first 4 links buttons added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import MyNavBar from "./components/MyNavBar";
 
 function App() {
-  return <></>;
+  return <MyNavBar />;
 }
 
 export default App;
