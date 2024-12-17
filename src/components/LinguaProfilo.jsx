e43910eab4d19855017ca866e9119086749971eb
Antonio Amormino <crisantoamo@gmail.com>
2024-12-17 15:53:54 +0100

    section-consigliati+linguaprofiloURLcreated

---
+++ src/components/LinguaProfilo.jsx
@@ -0,0 +1,32 @@
+import { Col, Container, Row } from "react-bootstrap";
+import { Pencil } from "react-bootstrap-icons";
+
+const LinguaProfilo = () => {
+  return (
+    <Container>
+      <Row className="flex-column border rounded-2 mb-1">
+        <Col>
+          <div className="border-bottom">
+            <div className="d-flex justify-content-between mt-3">
+              <h5 className="fs-6">Lingua del profilo</h5>
+              <Pencil />
+            </div>
+            <p className="fw-light">Italiano</p>
+          </div>
+        </Col>
+        <Col>
+          <div>
+            <div className="d-flex justify-content-between mt-3">
+              <h5 className="fs-6">Profilo pubblico e URL</h5>
+              <Pencil />
+            </div>
+            <p className="fw-light">
+              www.linkedin.com/in/nome-cognome-02155489
+            </p>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+export default LinguaProfilo;
