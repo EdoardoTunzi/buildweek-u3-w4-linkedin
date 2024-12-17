48465a9c6c86b2700debfd790c33f4cea3b39071
Antonio Amormino <crisantoamo@gmail.com>
2024-12-17 09:49:32 +0100

    footer completed + aside car users completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+// import CardUsers from "./components/CardUsers";
+import MyFooter from "./components/MyFooter";
 
 function App() {
-  return <></>;
+  return( <>
+  {/* <CardUsers/> */}
+  <MyFooter/>
+  </>);
 }
 
 export default App;
