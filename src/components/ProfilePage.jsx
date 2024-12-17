3186aa3e8fba3578db15e8cbb2141b6ec0ac3f5d
notdav04 <davide.alonzi26@gmail.com>
2024-12-17 15:30:13 +0100

    minor fixes

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -2,18 +2,13 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProfileCard from "./ProfileCard";
 import CardUsers from "./CardUsers";
 import MyFooter from "./MyFooter";
-import Analisi from "./Analisi";
-import Informazioni from "./Informazioni";
-import Attivita from "./Attivita";
+
 const ProfilePage = () => {
   return (
     <Container>
       <Row className="mt-4 mb-5 pb-5">
         <Col xs={9}>
           <ProfileCard />
-          <Analisi />
-          <Informazioni />
-          <Attivita />
         </Col>
         <Col xs={3}>
           <CardUsers />
